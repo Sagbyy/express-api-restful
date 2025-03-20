9cae89ee7041881c0840969f20a36bc61b24df24
Salahe-eddine BOUHDJEUR <salahe-eddine.bouhdjeur@horizontrading.io>
2025-03-20 22:38:10 +0100

    remove dist

--- dist/handlers/handler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const initHandler = (app) => {
-    app.get("/ping", (req, res) => {
-        res.send({ message: "pong" });
-    });
-};
-exports.default = initHandler;
